124024c1f8d0cdbd1d05c1327b7ec5c6b77e8ebe
kevinharv <kharvey2002@gmail.com>
2023-11-11 19:09:09 -0600

    Created Lambda function code; working on getting it to run

--- LAMBDA/go.mod
+++ LAMBDA/go.mod
@@ -1,3 +1,5 @@
 module LAMBDA/main
 
-go 1.21.3
+go 1.21
+
+require github.com/aws/aws-lambda-go v1.41.0 // indirect
--- LAMBDA/main.go
+++ LAMBDA/main.go
@@ -1,1 +1,15 @@
-// Lambda function
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func HandleRequest(ctx context.Context) (*string, error) {
+	message := "Hello World!"
+	return &message, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
